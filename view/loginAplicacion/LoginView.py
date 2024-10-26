f515928b688bfcb4586e48d1718fd74de53c178b
cristian <cregueiromartinez@danielcastelao.org>
2024-10-26 11:33:51 +0200

    diviendiendo app

---
+++ view/loginAplicacion/LoginView.py
@@ -0,0 +1,7 @@
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPalette, QColor
+from PyQt6.QtWidgets import (QMainWindow, QApplication, QVBoxLayout, QPushButton, QWidget, QLabel, QGridLayout)
+
+def layoutLogin():
+    layout = QGridLayout()
+    
